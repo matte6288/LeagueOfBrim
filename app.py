9ab2badb94db842e5115b0877678c59c2572a4a2
Matthew Ennis <matthewennis@Matthews-MBP-2.lan>
2020-11-07 15:00:19 -0500

    Got flask app to run able to search summoner name and need to pull stats and make it look nice

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import *
+import cassiopeia as cass
+
+
+
+cass.set_riot_api_key("RGAPI-9272180c-3f1a-4e02-ab76-7a2b337478b5")
+cass.set_default_region("NA")
+
+app = Flask(__name__)
+app.static_folder='static'
+
+
+@app.route('/')
+def default():
+    return redirect(url_for('summonerSearchPage'))
+
+@app.route('/summonerSearchPage', methods=['GET', 'POST'])
+def summonerSearchPage():
+    if request.method =="POST":
+        summonerName=request.form["SummonerName"]
+        return redirect(url_for('summonerRecap', sumName=summonerName))
+    else:
+        return render_template('summonerSearchPage.html')
+
+@app.route("/<sumName>")
+def summonerRecap(sumName):
+    summoner=cass.get_summoner(name=sumName)
+    return render_template('summonerRecap.html', name=sumName, level=summoner.level, rank=summoner.ranks, masteries=summoner.champion_masteries.pop(0).champion.name)
+
+
+if __name__ == '__main__':
+    app.run()
