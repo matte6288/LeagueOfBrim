85ddc558821496cd204366a3f01427c62c890216
Levi Putman <leviptm@gmail.com>
2020-11-14 16:05:15 -0500

    Ranked emblems

--- app.py
+++ app.py
@@ -31,6 +31,30 @@ def summonerRecap(sumName):
     ranked_flex = cass.data.Queue.ranked_flex_fives
     ranked_solo = cass.data.Queue.ranked_solo_fives
     rank = rank_data[cass.data.Queue.ranked_solo_fives]
+    rank = str(rank)
+    rank = rank.replace("<", "")
+    rank = rank.replace(">", "")
+    parsed_rank = rank.split(" ")
+    tier = parsed_rank[0]
+    if (tier == "Challenger"):
+        rank_url = "../static/ranked-emblems/Emblem_Challenger.png"
+    elif (tier == "Grandmaster"):
+        rank_url = "../static/ranked-emblems/Emblem_Grandmaster.png"
+    elif (tier == "Master"):
+        rank_url = "../static/ranked-emblems/Emblem_Master.png"
+    elif (tier == "Diamond"):
+        rank_url = "../static/ranked-emblems/Emblem_Diamond.png"
+    elif (tier == "Platinum"):
+        rank_url = "../static/ranked-emblems/Emblem_Platinum.png"
+    elif (tier == "Gold"):
+        rank_url = "../static/ranked-emblems/Emblem_Gold.png"
+    elif (tier == "Silver"):
+        rank_url = "../static/ranked-emblems/Emblem_Silver.png"
+    elif (tier == "Bronze"):
+        rank_url = "../static/ranked-emblems/Emblem_Bronze.png"
+    else:
+        rank_url = "../static/ranked-emblems/Emblem_Iron.png"
+
     icon=summoner.profile_icon.url
     bestChampImage=summoner.champion_masteries.copy().pop(0).champion.skins.copy().pop(0).splash_url
     champUrls=[]
@@ -40,7 +64,7 @@ def summonerRecap(sumName):
         champUrls.append(summoner.champion_masteries.pop(0).champion.skins.copy().pop(0).loading_image_url)
         champMasteriesLevels.append(champMasteriesCopy.pop(0).level)
     
-    return render_template('summonerRecap.html', name=name, level=level, rank=rank, bestChampImage=bestChampImage, icon=icon, champUrls=champUrls, champMasteriesLevels=champMasteriesLevels)
+    return render_template('summonerRecap.html', name=name, level=level, rank=rank, rankUrl=rank_url, bestChampImage=bestChampImage, icon=icon, champUrls=champUrls, champMasteriesLevels=champMasteriesLevels)
 
 
 if __name__ == '__main__':
