74e09f0b0854ca0ee50143015d7d7299118af4ab
Levi Putman <leviptm@gmail.com>
2020-11-29 16:38:35 -0500

    internal server bugfix + both ranks

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import cassiopeia as cass
 
 
 
-cass.set_riot_api_key("RGAPI-cc2f8249-4aff-4262-8d72-55621700082a")
+cass.set_riot_api_key("RGAPI-9777d270-c4fc-4ac1-a064-636894f3ad6c")
 cass.set_default_region("NA")
 
 app = Flask(__name__)
@@ -25,35 +25,60 @@ def summonerSearchPage():
 @app.route("/<sumName>")
 def summonerRecap(sumName):
     summoner=cass.get_summoner(name=sumName)
+    if not summoner.exists:
+        return render_template('summonerSearchPage.html')
     name=sumName
     level=summoner.level
     rank_data=summoner.ranks
-    ranked_flex = cass.data.Queue.ranked_flex_fives
-    ranked_solo = cass.data.Queue.ranked_solo_fives
-    rank = rank_data[cass.data.Queue.ranked_solo_fives]
-    rank = str(rank)
-    rank = rank.replace("<", "")
-    rank = rank.replace(">", "")
-    parsed_rank = rank.split(" ")
-    tier = parsed_rank[0]
-    if (tier == "Challenger"):
-        rank_url = "../static/ranked-emblems/Emblem_Challenger.png"
-    elif (tier == "Grandmaster"):
-        rank_url = "../static/ranked-emblems/Emblem_Grandmaster.png"
-    elif (tier == "Master"):
-        rank_url = "../static/ranked-emblems/Emblem_Master.png"
-    elif (tier == "Diamond"):
-        rank_url = "../static/ranked-emblems/Emblem_Diamond.png"
-    elif (tier == "Platinum"):
-        rank_url = "../static/ranked-emblems/Emblem_Platinum.png"
-    elif (tier == "Gold"):
-        rank_url = "../static/ranked-emblems/Emblem_Gold.png"
-    elif (tier == "Silver"):
-        rank_url = "../static/ranked-emblems/Emblem_Silver.png"
-    elif (tier == "Bronze"):
-        rank_url = "../static/ranked-emblems/Emblem_Bronze.png"
+    solo_rank = rank_data[cass.data.Queue.ranked_solo_fives]
+    solo_rank = str(solo_rank)
+    solo_rank = solo_rank.replace("<", "")
+    solo_rank = solo_rank.replace(">", "")
+    solo_parsed_rank = solo_rank.split(" ")
+    solo_tier = solo_parsed_rank[0]
+    if (solo_tier == "Challenger"):
+        solo_rank_url = "../static/ranked-emblems/Emblem_Challenger.png"
+    elif (solo_tier == "Grandmaster"):
+        solo_rank_url = "../static/ranked-emblems/Emblem_Grandmaster.png"
+    elif (solo_tier == "Master"):
+        solo_rank_url = "../static/ranked-emblems/Emblem_Master.png"
+    elif (solo_tier == "Diamond"):
+        solo_rank_url = "../static/ranked-emblems/Emblem_Diamond.png"
+    elif (solo_tier == "Platinum"):
+        solo_rank_url = "../static/ranked-emblems/Emblem_Platinum.png"
+    elif (solo_tier == "Gold"):
+        solo_rank_url = "../static/ranked-emblems/Emblem_Gold.png"
+    elif (solo_tier == "Silver"):
+        solo_rank_url = "../static/ranked-emblems/Emblem_Silver.png"
+    elif (solo_tier == "Bronze"):
+        solo_rank_url = "../static/ranked-emblems/Emblem_Bronze.png"
     else:
-        rank_url = "../static/ranked-emblems/Emblem_Iron.png"
+        solo_rank_url = "../static/ranked-emblems/Emblem_Iron.png"
+
+    flex_rank = rank_data[cass.data.Queue.ranked_flex_fives]
+    flex_rank = str(flex_rank)
+    flex_rank = flex_rank.replace("<", "")
+    flex_rank = flex_rank.replace(">", "")
+    flex_parsed_rank = flex_rank.split(" ")
+    flex_tier = flex_parsed_rank[0]
+    if (flex_tier == "Challenger"):
+        flex_rank_url = "../static/ranked-emblems/Emblem_Challenger.png"
+    elif (flex_tier == "Grandmaster"):
+        flex_rank_url = "../static/ranked-emblems/Emblem_Grandmaster.png"
+    elif (flex_tier == "Master"):
+        flex_rank_url = "../static/ranked-emblems/Emblem_Master.png"
+    elif (flex_tier == "Diamond"):
+        flex_rank_url = "../static/ranked-emblems/Emblem_Diamond.png"
+    elif (flex_tier == "Platinum"):
+        flex_rank_url = "../static/ranked-emblems/Emblem_Platinum.png"
+    elif (flex_tier == "Gold"):
+        flex_rank_url = "../static/ranked-emblems/Emblem_Gold.png"
+    elif (flex_tier == "Silver"):
+        flex_rank_url = "../static/ranked-emblems/Emblem_Silver.png"
+    elif (flex_tier == "Bronze"):
+        flex_rank_url = "../static/ranked-emblems/Emblem_Bronze.png"
+    else:
+        flex_rank_url = "../static/ranked-emblems/Emblem_Iron.png"
 
     icon=summoner.profile_icon.url
 
@@ -82,10 +107,10 @@ def summonerRecap(sumName):
     champMasteriesCopy2=summoner.champion_masteries.copy()
     champMasteriesCopy3=summoner.champion_masteries.copy()
     champMasteriesCopy4=summoner.champion_masteries.copy()
-    champMasteriesCopy5=summoner.champion_masteries.copy()
-    return render_template('summonerRecap.html', name=name, level=level, rank=rank, rankUrl=rank_url, \
+    return render_template('summonerRecap.html', name=name, level=level, soloRank=solo_rank, soloRankUrl=solo_rank_url, \
         icon=icon, champFullArtUrls=champFullArtUrls, champSplashUrls=champSplashUrls, champMasteryImageURLS=champMasteryImageURLS, \
-        champMasteriesPoints=champMasteriesPoints, champMasteryNames=champMasteryNames)
+        champMasteriesPoints=champMasteriesPoints, champMasteryNames=champMasteryNames, flexRank=flex_rank, flexRankUrl=flex_rank_url)
+
 
 
 if __name__ == '__main__':
